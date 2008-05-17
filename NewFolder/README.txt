77c3d866f60b77b9bbaa215c66536b93a7303b0d
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-05-17 13:56:20 +0000

    Initial import.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3dev/trunk@9654 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ NewFolder/README.txt
@@ -0,0 +1,4 @@
+
+The Icons are from http://www.famfamfam.com/lab/icons/silk/
+
+thx for this great icon set!
---
+++ NewFolder/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "t3dev"
+#
+# Auto generated 13-05-2008 23:33
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'T3dev',
+	'description' => 'links for developer from the good old extdeveval extension and some more links - but only for TYPO3 4.2 up',
+	'category' => 'be',
+	'author' => 'Frank Nägler',
+	'author_email' => 'typo3@naegler.net',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.1.0-0.0.0',
+			'typo3' => '4.2.0-4.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"f9e1";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"f98a";s:20:"doc/wizard_form.html";s:4:"75f0";}',
+);
+
+?>
